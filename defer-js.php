054fa8598f630c64ea31ab412013977115cfcc71
Himal Malith <dphmalith@gmail.com>
2021-06-04 21:03:29 +0530

    Create defer-js.php

---
+++ defer-js.php
@@ -0,0 +1,11 @@
+<?php
+
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
+
+?>
